a54b111eaf44cd9600021348bddc5261e6ed53e9
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2024-08-16 23:19:02 +0800

    Refine home page & revamp styling rules

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,21 @@ cards.forEach((el) => {
     target.classList.add("active");
   });
 });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("Header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
